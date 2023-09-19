dd6d3cfb875de67732e1da506737dc03a5463cd2
阿远 <qiuzongyuan0603@163.com>
2023-09-20 01:43:04 +0800

    feat: 转换变量名

--- src/VariableHelper.ts
+++ src/VariableHelper.ts
@@ -1,47 +1,92 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export default class VariableHelper {
-  constructor () {
+  constructor() {
     this.extractWords();
   }
-  words:string [] = [];
+  private words: string[] = [];
 
-  extractWords () {
+  extractWords() {
     const editor = vscode.window.activeTextEditor;
     if (!editor) {
       return [];
-    };
+    }
     const { selection } = editor;
-    const text = editor.document.getText(selection);//选择文本
-    const separator = ' ';
-    const symbolReg = /[\-_\s]+(.)?/g;
+    const text = editor.document.getText(selection); //选择文本
+    const separator = " ";
+    const charReg = /[A-Z][a-z]?/g;
     let convertText = text;
+    if (charReg.test(convertText)) {
+      convertText = convertText.replace(charReg, (match) => separator + match);
+    }
+    const symbolReg = /[\-_\s]+(.)?/g;
     if (symbolReg.test(text)) {
       convertText = convertText.replace(symbolReg, separator + "$1");
     }
-    const charReg = /[a-zA-Z][A-Z]/g;
-    if (charReg.test(convertText)) {
-      convertText = convertText.replace(charReg,  (match) => match[0] + separator + match[1]);
-    }
     convertText = convertText.toLocaleLowerCase();
-    this.words = convertText.split(separator);
+    this.words = convertText.split(separator).filter((word) => word !== "");
     console.log(this.words);
   }
 
-  replaceText (text: string) {
+  replaceText(text?: unknown) {
+    if (typeof text !== "string") return;
     const editor = vscode.window.activeTextEditor;
     if (!editor) {
       return;
-    };
+    }
     const { selection, edit } = editor;
     edit((builder) => {
-      builder.replace(selection, text);//替换选中文本
+      builder.replace(selection, text); //替换选中文本
     });
-  };
-
-
-  test() {
-    console.log('test');
   }
-  
+
+  camelCase() {
+    const words = this.words.map((word, index) => {
+      if (index > 0) {
+        return word.replace(/^[a-z]/, (match) => match.toLocaleUpperCase());
+      }
+      return word;
+    });
+    return words.join("");
+  }
+
+  snakeCase() {
+    return this.words.join("_");
+  }
+
+  kebabCase() {
+    return this.words.join("-");
+  }
+
+  pascalCase() {
+    const words = this.words.map((word) =>
+      word.replace(/^[a-z]/, (match) => match.toLocaleUpperCase())
+    );
+    return words.join("");
+  }
+
+  toUpperCase() {
+    return this.words.map((word) => word.toLocaleLowerCase());
+  }
+
+  screamingSnakeCase() {
+    return this.toUpperCase().join("_");
+  }
+
+  capitalizedSnakeCase() {
+    const words = this.words.map((word) =>
+      word.replace(/^[a-z]/, (match) => match.toLocaleUpperCase())
+    );
+    return words.join("_");
+  }
+
+  spaceLowerCase() {
+    return this.words.join(" ");
+  }
+
+  spaceUpperCase() {
+    return this.toUpperCase().join(" ");
+  }
 }
+
+export type VariableHelperKey = keyof VariableHelper;
--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,38 @@
-import * as vscode from 'vscode';
-import VariableHelper from './VariableHelper';
-const commands = [
-	{
-		name: 'convert',
-		fnName: 'test'
-	}
+import * as vscode from "vscode";
+import VariableHelper, { VariableHelperKey } from "./VariableHelper";
+
+const commands: VariableHelperKey[] = [
+  "camelCase",
+  "snakeCase",
+  "kebabCase",
+  "pascalCase",
+  "screamingSnakeCase",
+  "capitalizedSnakeCase",
+  "spaceLowerCase",
+  "spaceUpperCase",
 ];
-const handler = (fnName: typeof VariableHelper[keyof typeof VariableHelper]) => {
-	const helper = new VariableHelper;
-	helper[fnName]?.();
+
+const handler = (name: keyof VariableHelper) => {
+  const helper = new VariableHelper();
+  helper.extractWords();
+  const text = helper[name]?.();
+  helper.replaceText(text);
 };
 
 export function activate(context: vscode.ExtensionContext) {
-	commands.forEach((command) => {
-		const disposable = vscode.commands.registerCommand("convert", () => handler('test'));
-		context.subscriptions.push(disposable);
-	});
-	// 注册右键菜单命令
-// const disposable = vscode.commands.registerCommand('convert', () => {
-// 	// 在此处编写右键菜单命令的逻辑
-// 	// const words = extractWords();
-// 	// replaceText(words.join('_'));
-// 	// vscode.window.showInformationMessage();	
-// });
-
+  commands.forEach((command) => {
+    const disposable = vscode.commands.registerCommand(command, () =>
+      handler(command)
+    );
+    context.subscriptions.push(disposable);
+  });
+  // 注册右键菜单命令
+  // const disposable = vscode.commands.registerCommand('convert', () => {
+  // 	// 在此处编写右键菜单命令的逻辑
+  // 	// const words = extractWords();
+  // 	// replaceText(words.join('_'));
+  // 	// vscode.window.showInformationMessage();
+  // });
 }
 
 // This method is called when your extension is deactivated
