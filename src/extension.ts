7a381ece9a901b670bdfa2faff1dcb63e415fb6b
qiuzongyuan <qiuzongyuan0603@163.com>
2023-09-20 17:41:58 +0800

    feat: 优化代码

--- src/extension.ts
+++ src/extension.ts
@@ -14,7 +14,6 @@ const commands: VariableHelperKey[] = [
 
 const handler = (name: keyof VariableHelper) => {
   const helper = new VariableHelper();
-  helper.extractWords();
   const text = helper[name]?.();
   helper.replaceText(text);
 };
