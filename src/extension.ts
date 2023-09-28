32dccbbcc71c238eed7b02237c8c370f82d1919e
qiuzongyuan <qiuzongyuan0603@163.com>
2023-09-28 09:18:34 +0800

    feat:优化代码

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode";
 import VariableHelper, { VariableHelperKey } from "./VariableHelper";
-import { isString, isArray, hasOwn } from "./util";
+import { isNotEmptyString, isArray, hasOwn } from "./util";
 
 const commands: VariableHelperKey[] = [
   "camelCase",
@@ -28,24 +28,27 @@ const handler = async (name: keyof VariableHelper) => {
       const text = editor.document.getText(selection);
       const helper = new VariableHelper(text);
       const replaceText = helper[name]?.();
-       locations.forEach((location) => {
-        const {
-          range: {
-            start: { line: startLine, character: startCharacter },
-            end: { line: endLine, character: endCharacter },
-          },
-        } = location;
-        const code = startLine + startCharacter + endLine + endCharacter;
-        if (!codeSet.has(code) && isString(replaceText)) {
-          if (isArray(replaceTextMap[replaceText])) {
-            replaceTextMap[replaceText].push(new vscode.Range(startLine, startCharacter, endLine, endCharacter));
-          } else {
-            replaceTextMap[replaceText] = [new vscode.Range(startLine, startCharacter, endLine, endCharacter)];
-          }
-          codeSet.add(code);
+      locations.forEach((location) => {
+      const {
+        range: {
+          start: { line: startLine, character: startCharacter },
+          end: { line: endLine, character: endCharacter },
+        },
+      } = location;
+      const code = `${startLine}${startCharacter}${endLine}${endCharacter}`;
+      if (!codeSet.has(code) && isNotEmptyString(replaceText)) {
+        if (isArray(replaceTextMap[replaceText])) {
+          replaceTextMap[replaceText].push(new vscode.Range(startLine, startCharacter, endLine, endCharacter));
+        } else {
+          replaceTextMap[replaceText] = [new vscode.Range(startLine, startCharacter, endLine, endCharacter)];
         }
-      });
+        codeSet.add(code);
+      }
+    });
   }
+
+  console.log(replaceTextMap);
+  
 
   editor.edit((editBuilder) => {
     for (const replaceText in replaceTextMap) {
@@ -57,14 +60,6 @@ const handler = async (name: keyof VariableHelper) => {
       }
     }
   });
-
-  // editor.setDecorations(highlightDecorationType, matches);
-
-  // editor.edit((editBuilder) => {
-  //   matches.forEach(match => {
-  //     editBuilder.replace(match, "test");
-  //   });
-  // });
 };
 
 export function activate(context: vscode.ExtensionContext) {
--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,7 @@
 export const isString = (v:unknown): v is string => typeof v === 'string';
 
+export const isNotEmptyString = (v:unknown): v is string => isString(v) && v !== '';
+
 export const isArray = Array.isArray;
 
 export const hasOwn = (object: object, key: string | symbol) => Object.prototype.hasOwnProperty.call(object, key);
